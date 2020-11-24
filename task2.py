59a4ec0fd48acdb6522866be9dcbcd76612b025b
kittu1999 <49338854+kittu1999@users.noreply.github.com>
2020-11-24 20:03:31 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,23 @@
+#1.
+a=b=c=20
+print(a/10)
+print(b*50)
+print(c+60)
+
+#2.
+x='apple'
+a=list(x)
+a[2]='G'
+b=''.join(a)
+print(b)
+
+
+
+#3
+a=int(2)
+b=float(3.4)
+x=float(a)
+y=int(b)
+print(x,/n y)
+
+
