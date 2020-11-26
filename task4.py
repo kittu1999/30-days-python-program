5de0920fa47cf076b1008cb044d1aee684a803d8
kittu1999 <49338854+kittu1999@users.noreply.github.com>
2020-11-26 10:46:22 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,26 @@
+#1
+#a
+x=[1,2,3,4]
+x.append(1)
+print(x)
+#b
+del x[1]
+print(x)
+#c
+a=max(x)
+print(a)
+#d
+b=min(x)
+print(b)
+
+
+#2
+x=('a','b','c','d')
+a=reversed(x)
+print(tuple(a))
+
+
+#3
+x=(1,2,3,4,5)
+y=list(x)
+print(y)
