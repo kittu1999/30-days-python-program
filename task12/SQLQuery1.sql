d6a960a86fe7773e898b3a370c03b37ca7e890b4
kittu1999 <49338854+kittu1999@users.noreply.github.com>
2020-12-07 09:37:57 +0530

    Update SQLQuery1.sql

--- task12/SQLQuery1.sql
+++ task12/SQLQuery1.sql
@@ -1,2 +1,11 @@
+create database json_file
+CREATE TABLE person (
+    name char(20),
+    place char(50),
+    phone no int(20)
+    email_id varchar(50)
+    married boolean()
+    gender char(10),
+);
 SELECT * 
 FROM OPENROWSET (BULK 'C:\Users\diwak\Desktop\python\task12', SINGLE_CLOB) as import
